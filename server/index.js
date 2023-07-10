ed1671d3dbe970e7f71537244aa3a77abce08729
santo1827 <136288759+santo1827@users.noreply.github.com>
2023-07-10 14:10:52 +1000

    Commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ const con = mysql.createConnection({
   database:"gymApp"
 })
 
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:3000"
+}));
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
